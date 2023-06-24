793710ef62582be2472639fb778874d3451f49c0
Albert <albert.yu.zh@gmail.com>
2023-06-24 00:47:52 -0700

    update default route route config

--- .umirc.ts
+++ .umirc.ts
@@ -10,13 +10,25 @@ export default defineConfig({
   request: {},
   layout: {
     title: '好室友™平台',
+    locale: false,
   },
   favicons: ['https://www.haoshiyou.org/assets/res/icon.png'],
   routes: [
     {
       path: '/',
       component: './Home',
+      hideInMenu: true,
       // redirect: '/home',
+      // 不展示顶栏
+      headerRender: false,
+      // 不展示页脚
+      footerRender: false,
+      // 不展示菜单
+      menuRender: false,
+      // 不展示菜单顶栏
+      menuHeaderRender: false,
+      // 在面包屑中隐藏
+      hideInBreadcrumb: true,
     },
     {
       name: 'Search',
